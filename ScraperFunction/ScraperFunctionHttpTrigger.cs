20edecd51444d5e45f7ebcf6c1f14e69d53180d9
sasha radkova <sasharadkova@outlook.com>
2022-07-07 13:46:47 +0200

    Added a constructor to Function (need to implement DI)

--- ScraperFunction/ScraperFunctionHttpTrigger.cs
+++ ScraperFunction/ScraperFunctionHttpTrigger.cs
@@ -8,20 +8,31 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using ScraperWorker.Services;
+using Microsoft.Extensions.Hosting;
 
 // What I want to do:
 // When we go to localhost/api/ScraperFunctionHttpTrigger -  the ScrapeWebsiteAsync method runs
 
 namespace ScraperFunction
 {
-    public static class ScraperFunctionHttpTrigger
+    public class ScraperFunctionHttpTrigger
     {
+        // To support Dependency Injection via the constructor, we need the class to be instantiated with a constructor.
+        private readonly IScraperService _scraperService;
+        public ScraperFunctionHttpTrigger(IScraperService scraperService)
+        {
+            _scraperService = scraperService;
+        }
+
+
         [FunctionName(nameof(ScraperFunctionHttpTrigger))] // instead of using a string as a function name ("ScraperFunctionHttpTrigger"), you can use reference to the class
         public static async Task<IActionResult> Run(          // the Route is the url we have in Azure/api/Our function name (or localhost/api/functionname); if you set Route = "hello", it'll be whateverhost/api/hello
             [HttpTrigger(AuthorizationLevel.Function, "get",  Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
+
+
 
             string url = "https://www.xuntos.nl/";
             var scraperService = new ScraperService();
